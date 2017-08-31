50eb37b5656e231f2631b224c12b163c56476869
bwfadmin <bwfadmin@bwfadmin-PC>
2017-08-31 18:56:24 +0800

    gengxin

--- mybatis/src/com/bwf/bean/User.java
+++ mybatis/src/com/bwf/bean/User.java
@@ -8,7 +8,13 @@ public class User {
    
 	private int userId;
 
+	public int getUserId() {
+		return userId;
+	}
 
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
 	public String getUserName() {
 		return userName;
