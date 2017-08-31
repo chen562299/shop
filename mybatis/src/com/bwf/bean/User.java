499fa5074b4ad1d29a82f2f90d7831ddf6864526
bwfadmin <bwfadmin@bwfadmin-PC>
2017-08-31 18:54:53 +0800

    4

---
+++ mybatis/src/com/bwf/bean/User.java
@@ -0,0 +1,37 @@
+package com.bwf.bean;
+
+public class User {
+	
+	private String userName;
+	
+	private String userPsw;
+   
+	private int userId;
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPsw() {
+		return userPsw;
+	}
+
+	public void setUserPsw(String userPsw) {
+		this.userPsw = userPsw;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", userPsw=" + userPsw + "]";
+	}
+
+
+	
+
+}
