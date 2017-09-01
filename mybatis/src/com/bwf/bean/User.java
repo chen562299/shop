d5dbb796612998bd3460d3657f465f4e3c81bcc3
bwfadmin <bwfadmin@bwfadmin-PC>
2017-09-01 15:38:41 +0800

    修改user

--- mybatis/src/com/bwf/bean/User.java
+++ mybatis/src/com/bwf/bean/User.java
@@ -8,13 +8,7 @@ public class User {
    
 	private int userId;
 
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+	
 
 	public String getUserName() {
 		return userName;
