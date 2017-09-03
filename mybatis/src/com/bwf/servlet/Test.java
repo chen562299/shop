0093aab05b62e681f5f37df9cddb16c102cc60fb
bwfadmin <bwfadmin@bwfadmin-PC>
2017-09-03 17:14:58 +0800

    44444

--- mybatis/src/com/bwf/servlet/Test.java
+++ mybatis/src/com/bwf/servlet/Test.java
@@ -21,7 +21,6 @@ public class Test {
 
 
 			UserService userService=new UserServiceImpl();
-			
 
 			List<Oder> oders= userService.getSelectUserOder();
 			
@@ -29,32 +28,8 @@ public class Test {
 				System.out.println(oder.toString());
 				
 			}
-//			User adduser=new User();
-//			adduser.setUserName("222");
-//			adduser.setUserPsw("张");
-//			System.out.println(adduser.toString());
-//			if(userService.getAddUser(adduser)){
-//				System.out.println("添加成功");
-//			}else{
-//				System.out.println("添加失败");
-//			};
-			
-			
-			
-			
-			
 
-		
-			
-			//sqls.close();
-			//List<User> users= MybatisUtil.getOpenSession().selectList("my.getUserSize");
-			//System.out.println(users.size());
-			
-			//User user= (User)sqs.selectOne("my.getUserName",1);
-			//System.out.println(user.getUserName());
-			
-			//sqls.close();
-			
+	
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
